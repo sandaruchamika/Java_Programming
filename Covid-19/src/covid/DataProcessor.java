e7143768e6f3656b96fbf398c54a8a63ea93ee53
sandaruchamika <chamikasandaru01@gmail.com>
2020-05-21 22:51:08 +0530

    file path changed

--- Covid-19/src/covid/DataProcessor.java
+++ Covid-19/src/covid/DataProcessor.java
@@ -1,7 +1,7 @@
 package covid;
 
 public class DataProcessor {
-	private static final String FILE_PATH = "/Users/Acer/eclipse-workspace/Covid-19/src/covid/covid-19-data.json";
+	private static final String FILE_PATH = "\\Users\\Acer\\git\\Java_Programming\\Covid-19\\src\\covid\\covid-19-data.json";
 	
 	public static void main(String[] args) {
 		 DataReader reader = new DataReader();
