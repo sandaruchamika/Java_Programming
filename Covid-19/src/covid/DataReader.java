1f8f056c917b170c221d10a685b61c757d4ee2df
sandaruchamika <chamikasandaru01@gmail.com>
2020-05-21 22:45:38 +0530

    added files for Task 01

---
+++ Covid-19/src/covid/DataReader.java
@@ -0,0 +1,39 @@
+package covid;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class DataReader {
+	private static final String JSON_ARRAY = "records";
+    private static final String COUNTRY_CODE = "countryterritoryCode";
+    private static final String DATE = "dateRep";
+    private static final String CASES = "cases";
+    private static final String DEATHS = "deaths";
+    
+    /**
+     * This method reads the contents form a JSON file and prints the required data.
+     * 
+     * @param filePath the path to JSON file.
+     */
+    public void readData(String filePath) {
+        try {
+        	String fileContent = new String(Files.readAllBytes(Paths.get(filePath)));
+            JSONObject obj  = new JSONObject(fileContent);
+            JSONArray arr = obj.getJSONArray(JSON_ARRAY);
+            
+            System.out.println("\tDate \t|\tReported Cases\t|\tDeaths");
+            
+            for(int i = 0; i < arr.length(); i ++) {
+            	if (arr.getJSONObject(i).getString(COUNTRY_CODE).equals("LKA")){
+            		System.out.println("    " + arr.getJSONObject(i).getString(DATE) + "\t\t  " + arr.getJSONObject(i).getString(CASES) + "\t\t\t " + arr.getJSONObject(i).getString(DEATHS));
+            	}
+            }    
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
