5e2f29d440f62f9b77bda701c41c2b8ae618ca6b
sandaruchamika <chamikasandaru01@gmail.com>
2020-05-23 01:42:57 +0530

    defined a constant for selecting the country

--- Covid-19/src/covid/DataReader.java
+++ Covid-19/src/covid/DataReader.java
@@ -14,6 +14,8 @@ public class DataReader {
     private static final String CASES = "cases";
     private static final String DEATHS = "deaths";
     
+    private static final String SELECTED_COUNTRY_CODE  = "LKA";
+    
     /**
      * This method reads the contents form a JSON file and prints the required data.
      * 
@@ -28,7 +30,7 @@ public class DataReader {
             System.out.println("\tDate \t|\tReported Cases\t|\tDeaths");
             
             for(int i = 0; i < arr.length(); i ++) {
-            	if (arr.getJSONObject(i).getString(COUNTRY_CODE).equals("LKA")){
+            	if (arr.getJSONObject(i).getString(COUNTRY_CODE).equals(SELECTED_COUNTRY_CODE)){
             		System.out.println("    " + arr.getJSONObject(i).getString(DATE) + "\t\t  " + arr.getJSONObject(i).getString(CASES) + "\t\t\t " + arr.getJSONObject(i).getString(DEATHS));
             	}
             }    
