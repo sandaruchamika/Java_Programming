9b8d007bfbef001d5c60c60eec6b628be9400846
sandaruchamika <chamikasandaru01@gmail.com>
2020-05-28 16:26:15 +0530

    Added required code blocks

---
+++ Task 03/src/lk/ac/kln/fct/learn/reflection/main/Main.java
@@ -0,0 +1,21 @@
+package lk.ac.kln.fct.learn.reflection.main;
+
+import java.lang.reflect.InvocationTargetException;
+import lk.ac.kln.fct.learn.reflection.shapes.Rectangle;
+import java.lang.reflect.Field;
+
+public class Main {
+	public static void main(String[] args) {
+		Rectangle rectangle = null;
+		try {
+			Class<?> clazz = Rectangle.class;
+			rectangle = (Rectangle) clazz.getConstructor().newInstance();
+			Field field = clazz.getSuperclass().getDeclaredField("name");
+			field.setAccessible(true);
+			field.set(rectangle, "Rectangle");
+			} catch (Exception e) {			
+			e.printStackTrace();
+		}
+		System.out.println("Shape name: "+ rectangle.getName());
+	}
+}
---
+++ Task 03/src/lk/ac/kln/fct/learn/reflection/shapes/Rectangle.java
@@ -0,0 +1,8 @@
+package lk.ac.kln.fct.learn.reflection.shapes;
+
+public class Rectangle extends Shape{
+	@Override
+	public double area(double l1, double l2) {
+		return l1 * l2;
+	}	
+}
