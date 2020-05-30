cab871116404d295afcb7a008976aa671bbd015c
sandaruchamika <chamikasandaru01@gmail.com>
2020-05-31 04:54:15 +0530

    Revert "Fixed getEntryByKey method"
    This reverts commit 9f4bd6a3d8c432d81282474c584749d7d60e035d.

--- Task 05/src/lk/ac/kln/fct/learn/generics/core/Inventory.java
+++ Task 05/src/lk/ac/kln/fct/learn/generics/core/Inventory.java
@@ -1,7 +1,7 @@
 package lk.ac.kln.fct.learn.generics.core;
 
 import java.util.ArrayList;
-
+import java.util.NoSuchElementException;
 
 public class Inventory<K, V> {
 	private ArrayList<Entry<K, V>> entries = new ArrayList<>();
@@ -29,9 +29,7 @@ public class Inventory<K, V> {
 				return e;
 			}
 		}
-		return null;
-//		return entries.stream().filter((entry) -> entry.getKey().equals(key)).findAny().get();
-
+		throw new NoSuchElementException();
 	}
 
 	public int getEntryIndex(Entry<K, V> entry) {
