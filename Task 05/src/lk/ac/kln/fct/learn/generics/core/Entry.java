26da5ab4638d2d591219cd6212c679300e093ad7
sandaruchamika <chamikasandaru01@gmail.com>
2020-05-31 03:09:34 +0530

    Added generics and required code blocks

---
+++ Task 05/src/lk/ac/kln/fct/learn/generics/core/Entry.java
@@ -0,0 +1,26 @@
+package lk.ac.kln.fct.learn.generics.core;
+
+public class Entry<K, V> {
+	private K key;
+	private V value;
+	
+	public Entry(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public V getValue() {
+		return value;
+	}
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+	
+}
---
+++ Task 05/src/lk/ac/kln/fct/learn/generics/core/Inventory.java
@@ -0,0 +1,50 @@
+package lk.ac.kln.fct.learn.generics.core;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class Inventory<K, V> {
+	private ArrayList<Entry<K, V>> entries = new ArrayList<>();
+	
+	public void addToInventory(Entry<K, V> entry) {
+		for(Entry<K, V> e: entries) {
+		    if(e.getKey().equals(entry.getKey())) {
+		    	throw new IllegalArgumentException("Duplicate key found");
+			}
+		}
+		entries.add(entry);
+	}
+
+	public ArrayList<Entry<K, V>> getInventory() {
+		return entries;
+	}	
+	
+	public Entry<K, V> getEntryByIndex(int index) {
+		return entries.get(index);
+	}
+
+	public Entry<K, V> getEntryByKey(K key) {
+		for(Entry<K, V> e : entries) {
+			if(e.getKey().equals(key)) {
+				return e;
+			}
+		}
+		throw new NoSuchElementException();
+	}
+
+	public int getEntryIndex(Entry<K, V> entry) {
+		return entries.indexOf(entry);
+	}
+	
+	public boolean removeByEntry(Entry<K, V> entry) {
+		return entries.remove(entry);
+	}
+	
+	public Entry<K, V> removeByIndex(int index) {
+		return entries.remove(index);
+	}
+	
+	public void removeAll() {
+		entries.clear();
+	}
+}
